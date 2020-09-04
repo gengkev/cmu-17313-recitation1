52fe6b4479480f70f16e6ec99b348968003f5772
shreyavarra <shreyavarra@gmail.com>
2020-09-04 11:18:23 -0700

    add name to java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world, Kevin Geng! I've been at CMu for 4 years.");
+    System.out.println("Hello world, Kevin Geng! I've been at CMU for 4 years.");
+    System.out.println("Hello! This is Shreya!");
   }
 }
