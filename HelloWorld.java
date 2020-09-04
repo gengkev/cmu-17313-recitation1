bf3cc148ad34829f4e874ee401807e1ff9e437ed
Kevin Geng <khg@andrew.cmu.edu>
2020-09-04 13:50:10 -0400

    Add how many years I've been at CMU

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello world! I've been at CMu for 4 years.");
   }
 }
